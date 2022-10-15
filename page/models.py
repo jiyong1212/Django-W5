3e9bc8742c0ddccb0a72fdebef91d1792115882b
jiyong1212 <jiyongpark0215@gmail.com>
2022-10-15 17:45:43 +0900

    W5

--- page/models.py
+++ page/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 # [코드 작성] Post 모델 작성
+class post(models.Model):
+    title = models.CharField(max_length = 50)
+    context = models.TextField()
+    date = models.DateTimeField(auto_now_add = True)
+    
